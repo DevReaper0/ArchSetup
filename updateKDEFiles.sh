fead42aec8d2d7d8d91242df6ade59ae69affd93
DaRubyMiner360 <darubyminer360@gmail.com>
2023-04-26 21:48:02 -0500

    Added KDE wallpapers

--- updateKDEFiles.sh
+++ updateKDEFiles.sh
@@ -5,6 +5,7 @@ mkdir kde/
 cd kde/
 mkdir -p usr/share/
 sudo cp -rf /usr/share/plasma/ usr/share/plasma/
+sudo cp -rf /usr/share/wallpapers/ usr/share/wallpapers/
 mkdir -p \$USER/.local/share/
 sudo cp -rf ~/.local/share/plasma/ \$USER/.local/share/plasma/
 sudo cp -rf ~/.local/share/latte-layouts/ \$USER/.local/share/latte-layouts/
